dec7144f1a83f11053aa88c48410a94f57abea3e
romanpovzyk <roman.povzyk@gmail.com>
2021-06-07 15:00:38 +0300

    day #15/222

---
+++ Section 21. ML + Data Science/301.py
@@ -0,0 +1,12 @@
+from imageai.Prediction import ImagePrediction
+import os
+execution.path=os.getcwd()
+
+prediction=ImagePrediction()
+prediction.setModelTypeAsResNet50()
+prediction.setModelPath(os.path.join(execution_path, "resnet50_imagenet_tf.2.0.h5"))
+prediction.loadModel()
+
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "car.jpg"), result_count=5 )
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , " : " , eachProbability)
